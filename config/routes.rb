7a0cb78a8a701382c1491224270113f86c59ac20
hamadweb <hamadkhaliq728@gmail.com>
2017-09-27 19:10:03 +0500

    added custom actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
   resources :tasks
   resources :tasks
   resources :tasks
